6f2a237643dc7d8b6b48104a69b03f69f3d04fdf
lca <545404369@qq.com>
2021-06-25 16:58:29 +0800

    first commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+
+/**
+ * @Project: jenkins_test
+ * @Author: 54540
+ * @Create: 2021-06-21 17:13
+ * @Desc：
+ **/
+public class LoginTest {
+   @Test
+   public void setup(){
+      System.out.println("登录成功");
+   }
+}
---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+
+/**
+ * @Project: jenkins_test
+ * @Author: 54540
+ * @Create: 2021-06-21 17:13
+ * @Desc：
+ **/
+public class RegisterTest {
+   @Test
+   public void setup(){
+      System.out.println("注册成功");
+   }
+}
